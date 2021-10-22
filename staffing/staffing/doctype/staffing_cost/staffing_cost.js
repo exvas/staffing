8f7caba4d9b2d955557b54a995ed82d24364cf30
jiloysss <janlloydangeles@gmail.com>
2021-10-23 00:26:54 +0800

    update

--- staffing/staffing/doctype/staffing_cost/staffing_cost.js
+++ staffing/staffing/doctype/staffing_cost/staffing_cost.js
@@ -10,5 +10,30 @@ frappe.ui.form.on('Staffing Cost', {
                 ]
             }
         })
-	}
+	},
+    default_cost_rate_per_hour: function(frm) {
+       if(cur_frm.doc.default_cost_rate_per_hour > cur_frm.doc.default_billing_rate_per_hour){
+           cur_frm.doc.default_cost_rate_per_hour = 0
+           cur_frm.refresh_field("default_cost_rate_per_hour")
+           frappe.throw("Default Cost Rate Per Hour must be lesser than or equal to Default Billing Rate Per Hour")
+       }
+	},
+    friday_working_costing_rate: function(frm) {
+       if(cur_frm.doc.friday_working_costing_rate > cur_frm.doc.friday_working_billing_rate){
+           cur_frm.doc.friday_working_costing_rate = 0
+           cur_frm.refresh_field("default_cost_rate_per_hour")
+           frappe.throw("Friday Working Costing Rate must be lesser than or equal to Friday Working Billing Rate")
+       }
+	},
+    reference_type: function () {
+        if(cur_frm.doc.reference_type === "Employee"){
+            staff_code = ""
+            staff_name = ""
+            cur_frm.refresh_fields(["staff_code", "staff_name"])
+        } else if(cur_frm.doc.reference_type === "Staff"){
+            employee_code = ""
+            employee_name = ""
+            cur_frm.refresh_fields(["employee_name", "employee_code"])
+        }
+    }
 });
--- staffing/staffing/doctype/timesy/timesy.js
+++ staffing/staffing/doctype/timesy/timesy.js
@@ -25,7 +25,20 @@ frappe.ui.form.on('Timesy', {
                     }, () => {})
 
         }
-
+         cur_frm.set_query("employee_code", () => {
+            return {
+                filters: {
+                    status: 'Active'
+                }
+            }
+        })
+        cur_frm.set_query("staff_code", () => {
+            return {
+                filters: {
+                    status: 'Active'
+                }
+            }
+        })
          cur_frm.set_query("staffing_project", "timesy_details", () => {
             return {
                 filters: {
@@ -40,48 +53,129 @@ frappe.ui.form.on('Timesy', {
                 ]
             }
         })
+	},
+	staff_code: function(frm) {
+	    if(cur_frm.doc.staff_code){
+	         var obj = {
+                    staff_code: cur_frm.doc.staff_code,
+                    docstatus: 1
+                }
+	        get_designation(cur_frm, obj)
+        } else {
+	        cur_frm.doc.designation = ""
+                cur_frm.refresh_field("designation")
+        }
+
+	},
+    employee_code: function(frm) {
+      if(cur_frm.doc.employee_code){
+          var obj = {
+              employee_code: cur_frm.doc.employee_code,
+                    docstatus: 1
+          }
+	        get_designation(cur_frm, obj)
+        } else {
+	        cur_frm.doc.designation = ""
+	        cur_frm.doc.staffing_type = ""
+	        cur_frm.doc.staffing_project = ""
+            cur_frm.refresh_field("designation")
+        }
+
 	}
-});
+})
+function get_designation(cur_frm, obj) {
+     frappe.db.count('Staffing Cost', obj)
+            .then(count => {
+               if(count > 0){
+                    frappe.db.get_value('Staffing Cost', obj,  ["name", "staffing_project", "demobilization_date"])
+                        .then(r => {
+                            let values = r.message;
+                            console.log(values)
+                            cur_frm.doc.staffing_type = values.name
+                            cur_frm.doc.staffing_project = values.staffing_project
+                            cur_frm.doc.demobilization_date = values.demobilization_date
+                            cur_frm.refresh_fields(["staffing_type","staffing_project", "demobilization_date"])
+
+
+                        })
+
+                }
+            })
+}
 frappe.ui.form.on('Timesy Details', {
     working_hour: function(frm, cdt, cdn) {
         var d = locals[cdt][cdn]
-        if(d.staffing_type){
-            frappe.db.get_doc("Staffing Cost", d.staffing_type)
-                .then(doc => {
-                    d.costing_hour = doc.default_cost_rate_per_hour * d.working_hour
-                    d.billing_hour = doc.default_billing_rate_per_hour * d.working_hour
-                    d.absent_hour = doc.absent_deduction_per_hour * d.working_hour
-                    cur_frm.refresh_field(d.parentfield)
-                    total_costing(cur_frm)
-            })
+        if(cur_frm.doc.staffing_type){
+           compute_hours(d,cur_frm)
         }
 	},
-    staffing_type: function(frm, cdt, cdn) {
-         var d = locals[cdt][cdn]
-        if(d.staffing_type){
-            frappe.db.get_doc("Staffing Cost", d.staffing_type)
-                .then(doc => {
-                    d.costing_hour = doc.default_cost_rate_per_hour * d.working_hour
-                    d.billing_hour = doc.default_billing_rate_per_hour * d.working_hour
-                    d.absent_hour = doc.absent_deduction_per_hour * d.working_hour
-
-                    cur_frm.refresh_field(d.parentfield)
-            })
+    status: function(frm, cdt, cdn) {
+        var d = locals[cdt][cdn]
+        if(cur_frm.doc.staffing_type){
+           compute_hours(d,cur_frm)
         }
-        total_costing(cur_frm)
 	}
 });
+function compute_hours(d,cur_frm) {
+    frappe.db.get_doc("Staffing Cost", cur_frm.doc.staffing_type)
+        .then(doc => {
+            if(d.status === "Absent"){
+                d.costing_hour = 0
+                d.billing_hour = 0
+                d.absent_hour = doc.absent_deduction_per_hour * d.working_hour
+                d.friday_hour = 0
+                cur_frm.refresh_field(d.parentfield)
+                total_costing(cur_frm)
+            } else  if(d.status === "Medical"){
+                d.costing_hour = doc.default_cost_rate_per_hour * d.working_hour
+                d.billing_hour = doc.default_billing_rate_per_hour * d.working_hour
+                d.absent_hour = 0
+                d.friday_hour = doc.friday_working_costing_rate * d.working_hour
+
+                cur_frm.refresh_field(d.parentfield)
+                total_costing(cur_frm)
+            } else  if(d.status === "Friday Working"){
+                d.costing_hour =0
+                d.billing_hour = 0
+                d.absent_hour = 0
+                        d.friday_hour = doc.friday_working_costing_rate * d.working_hour
+
+                cur_frm.refresh_field(d.parentfield)
+                total_costing(cur_frm)
+            } else  if(d.status === "Friday"){
+                d.costing_hour = 0
+                d.billing_hour = 0
+                d.absent_hour = 0
+                                d.friday_hour = 0
+
+                cur_frm.refresh_field(d.parentfield)
+                total_costing(cur_frm)
+            } else  if(d.status === "Working"){
+                d.costing_hour = doc.default_cost_rate_per_hour * d.working_hour
+                d.billing_hour = doc.default_billing_rate_per_hour * d.working_hour
+                d.absent_hour = 0
+                d.friday_hour = 0
+
+                cur_frm.refresh_field(d.parentfield)
+                total_costing(cur_frm)
+            }
+
+    })
+}
 function total_costing(cur_frm) {
     var total_costing_hour = 0
     var total_billing_hour = 0
     var total_absent_hour = 0
+    var total_friday_hour = 0
     for(var x=0;x<cur_frm.doc.timesy_details.length;x+=1){
         total_costing_hour += cur_frm.doc.timesy_details[x].costing_hour
         total_billing_hour += cur_frm.doc.timesy_details[x].billing_hour
         total_absent_hour += cur_frm.doc.timesy_details[x].absent_hour
+        total_friday_hour += cur_frm.doc.timesy_details[x].friday_hour
     }
     cur_frm.doc.total_costing_hour = total_costing_hour
     cur_frm.doc.total_billing_hour = total_billing_hour
     cur_frm.doc.total_absent_hour = total_absent_hour
-    cur_frm.refresh_fields(['total_costing_hour','total_billing_hour','total_absent_hour'])
+    cur_frm.doc.total_friday_hour = total_friday_hour
+    cur_frm.refresh_fields(['total_costing_hour','total_billing_hour','total_absent_hour', 'total_friday_hour'])
 }
