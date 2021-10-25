cd048f5f9f4ad6a9e3946e98c2b922e23e9c4470
jiloysss <janlloydangeles@gmail.com>
2021-10-25 21:50:21 +0800

    update

--- staffing/staffing/doctype/mobilization_entry/mobilization_entry.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, jan and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Mobilization Entry', {
-	// refresh: function(frm) {
-
-	// }
-});
--- staffing/staffing/doctype/mobilization_entry/test_mobilization_entry.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, jan and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestMobilizationEntry(unittest.TestCase):
-	pass
