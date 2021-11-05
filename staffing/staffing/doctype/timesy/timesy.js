3ee61ba5451d05695a617aa76d0a98b29c637c6b
jiloysss <janlloydangeles@gmail.com>
2021-11-05 22:35:39 +0800

    update

--- staffing/staffing/doctype/timesy/timesy.js
+++ staffing/staffing/doctype/timesy/timesy.js
@@ -131,7 +131,14 @@ function compute_total_values(cur_frm,normal_working_hour, absent,overtime_hour)
               .then(doc => {
                 cur_frm.doc.total_costing_hour = (doc.default_cost_rate_per_hour * normal_working_hour) - (doc.absent_deduction_per_hour * absent)
                cur_frm.doc.total_absent_hour = type !== 'Absent' ?  doc.absent_deduction_per_hour * absent : 0
-               cur_frm.doc.total_overtime_hour = doc.default_overtime_rate && doc.default_overtime_rate > 0 ? (doc.default_overtime_rate * overtime_hour) - (doc.absent_deduction_per_hour * absent) : 0
+             var overtime_rate = 0
+            if(absent > 0){
+                    cur_frm.doc.total_overtime_hour = (overtime_hour * doc.default_cost_rate_per_hour) - doc.absent_deduction_per_hour
+            } else {
+                 cur_frm.doc.total_overtime_hour = doc.default_overtime_rate && doc.default_overtime_rate > 0 ?
+                   (doc.default_overtime_rate * overtime_hour) - (doc.absent_deduction_per_hour * absent) : 0
+            }
+
              cur_frm.refresh_fields(["total_costing_hour",'total_absent_hour'])
         })
 }
