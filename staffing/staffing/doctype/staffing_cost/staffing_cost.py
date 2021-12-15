6c917fa967517fa6e777aec3be1089570d1937f9
jiloysss <janlloydangeles@gmail.com>
2021-12-15 18:33:40 +0800

    update

--- staffing/staffing/doctype/staffing_cost/staffing_cost.py
+++ staffing/staffing/doctype/staffing_cost/staffing_cost.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2021, jan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class StaffingCost(Document):
-	pass
+	def validate(self):
+		if self.default_cost_rate_per_hour == 0:
+			frappe.throw("Default Cost Rate Per Hour must be greater than 0")
+
+		if self.reference_type == 'Staff' and self.default_billing_rate_per_hour == 0:
+			frappe.throw("Default Billing Rate Per Hour must be greater than 0")
