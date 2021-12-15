f2d5898e86235974634ec4ce2b6276f47e4ee3ed
jiloysss <janlloydangeles@gmail.com>
2021-12-15 23:46:38 +0800

    update

--- staffing/staffing/report/employee_timesheet/employee_timesheet.py
+++ staffing/staffing/report/employee_timesheet/employee_timesheet.py
@@ -51,7 +51,7 @@ def execute(filters=None):
 				x[str(xx.day_of_the_month)] = xx.working_hour
 
 		x['total_hour'] = sum
-		x['amount'] = x.default_billing_rate_per_hour * sum
+		x['amount'] = x.default_cost_rate_per_hour * sum
 		x['absent'] = absent
 		x['total_absent_deduction_per_hour'] = absent * x.absent_deduction_per_hour
 		x['net_total'] = x['amount'] - x['total_absent_deduction_per_hour']
