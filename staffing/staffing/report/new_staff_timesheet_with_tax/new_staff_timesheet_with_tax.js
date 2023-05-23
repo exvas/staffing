97db13463ac2e6ab79c6fb34b888d822adec329b
ranjanakmomscode <ranjana.k@momscode.in>
2023-05-23 06:08:16 +0530

    updated

---
+++ staffing/staffing/report/new_staff_timesheet_with_tax/new_staff_timesheet_with_tax.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, jan and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["New Staff Timesheet with Tax"] = {
+	"filters": [
+
+	]
+};
