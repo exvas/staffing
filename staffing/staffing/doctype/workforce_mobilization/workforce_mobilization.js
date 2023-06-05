e36e4a633d8f1cdee992a2da0466445bb0ea5e89
ranjanakmomscode <ranjana.k@momscode.in>
2023-06-05 23:19:30 +0530

    updated

--- staffing/staffing/doctype/workforce_mobilization/workforce_mobilization.js
+++ staffing/staffing/doctype/workforce_mobilization/workforce_mobilization.js
@@ -2,7 +2,42 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Workforce Mobilization', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		cur_frm.set_query("reference_type", (frm) => {
+            return {
+                filters: {
+					name: ["in", ['Employee', 'Staff']]
+                }
+			}
+        })
+		if (cur_frm.doc.demobilization_date){
+			cur_frm.set_df_property('demobilization_date','read_only', 1);
+		}
+		if (cur_frm.doc.release_date){
+			cur_frm.set_df_property('release_date','read_only', 1);
+		}
+		if(frm.docstatus=1){
+			frm.add_custom_button(__("Update Demobilization Date"), () => { 
+				if(! cur_frm.doc.release_date){
+					cur_frm.set_df_property('demobilization_date','read_only', 0);
+					cur_frm.refresh_field("demobilization_date")
+					frm.set_value('status',"Standby")
+				}
+                
+            },'Update')
 
-	// }
+			frm.add_custom_button(__("Update Release Date"), () => { 
+				if(! cur_frm.doc.demobilization_date){
+					cur_frm.set_df_property('release_date','read_only', 0);
+					cur_frm.refresh_field("release_date")
+					frm.set_value('status',"Released")
+				}
+                
+            },'Update')
+		
+		}
+
+	},
+	
+	
 });
---
+++ staffing/staffing/doctype/workforce_mobilization/workforce_mobilization_list.js
@@ -0,0 +1,22 @@
+frappe.listview_settings['Workforce Mobilization'] = {
+    add_fields:["status"],
+    get_indicator:function(doc){
+      if (doc.status === "Active") {
+        return [("Active"), "green", "status,=,Active"];      
+      }
+      else if(doc.status === "Standby"){
+        return [("Standby"), "red", "status,=,Standby"];
+  
+      }
+	  else if(doc.status === "Released"){
+        return [("Released"), "orange", "status,=,Released"];
+  
+      }
+      
+    },
+    
+  
+    
+  
+      
+  };
