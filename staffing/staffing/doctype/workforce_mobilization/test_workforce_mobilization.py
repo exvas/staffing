0d21a2713363caf62046cdcda9d4e3cae0fa6d3f
sammish <sammish.thundiyil@gmail.com>
2023-06-05 20:59:54 +0530

    doctype

---
+++ staffing/staffing/doctype/workforce_mobilization/test_workforce_mobilization.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, jan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWorkforceMobilization(FrappeTestCase):
+	pass
